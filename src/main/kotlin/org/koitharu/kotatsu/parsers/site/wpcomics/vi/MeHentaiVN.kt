50f8a9b704e30ed4233977e8076b682bcf811bf7
dragonx943 <premieregirl26@gmail.com>
2024-12-28 08:28:25 +0000

    [MeHentaiVN] Fix attempt 4

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/wpcomics/vi/MeHentaiVN.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/wpcomics/vi/MeHentaiVN.kt
@@ -1,6 +1,8 @@
 package org.koitharu.kotatsu.parsers.site.wpcomics.vi
 
+import kotlinx.coroutines.async
 import androidx.collection.ArraySet
+import kotlinx.coroutines.coroutineScope
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.config.ConfigKey
@@ -20,6 +22,40 @@ internal class MeHentaiVN(context: MangaLoaderContext) :
 		availableTags = fetchTags(),
 		availableStates = EnumSet.of(MangaState.ONGOING, MangaState.FINISHED),
 	)
+
+    override suspend fun getDetails(manga: Manga): Manga = coroutineScope {
+		val fullUrl = manga.url.toAbsoluteUrl(domain)
+		val doc = webClient.httpGet(fullUrl).parseHtml()
+		val chaptersDeferred = async { getChapters(doc) }
+		val tagMap = getOrCreateTagMap()
+		val tagsElement = doc.select("li.kind p.col-xs-8 a")
+        val mangaTags = tagsElement.mapNotNullToSet { 
+            val tagTitle = it.text()
+            if (tagTitle.isNotEmpty())
+                MangaTag(
+                    title = tagTitle,
+                    key = tagsElement.attr("href").substringAfterLast('/').trim(),
+                    source = source
+                )
+            else null
+        }
+
+		manga.copy(
+			description = doc.selectFirst(selectDesc)?.html().orEmpty(),
+			altTitle = doc.selectFirst("h2.other-name")?.text().orEmpty(),
+			author = doc.body().select(selectAut).text(),
+			state = doc.selectFirst(selectState)?.let {
+				when (it.text()) {
+					in ongoing -> MangaState.ONGOING
+					in finished -> MangaState.FINISHED
+					else -> null
+				}
+			},
+			tags = mangaTags,
+			rating = doc.selectFirst("div.star input")?.attr("value")?.toFloatOrNull()?.div(5f) ?: RATING_UNKNOWN,
+			chapters = chaptersDeferred.await(),
+		)
+	}
     
     private suspend fun fetchTags(): Set<MangaTag> {
         val doc = webClient.httpGet("https://$domain/").parseHtml()
