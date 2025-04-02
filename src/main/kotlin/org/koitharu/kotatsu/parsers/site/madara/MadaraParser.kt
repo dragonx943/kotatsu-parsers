9121658e86e24b13f789ca051b324bca10f890e6
dragonx943 <premieregirl26@gmail.com>
2025-04-02 20:54:30 +0700

    [Madara] Debug attempt 1

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/MadaraParser.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/MadaraParser.kt
@@ -626,6 +626,11 @@ internal abstract class MadaraParser(
 			webClient.httpPost(url, emptyMap()).parseHtml()
 		}
 		val dateFormat = SimpleDateFormat(datePattern, sourceLocale)
+
+		if (doc.select(selectChapter).isEmpty()) {
+			throw Exception(doc.toString()) // for debug
+		}
+
 		return doc.select(selectChapter).mapChapters(reversed = true) { i, li ->
 			val a = li.selectFirstOrThrow("a")
 			val href = a.attrAsRelativeUrl("href")
