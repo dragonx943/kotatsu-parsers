2ea5f1c5f8edcb696aca70ae3afd955c6b9dccc3
dragonx943 <premieregirl26@gmail.com>
2025-06-19 02:46:21 +0700

    [AgsComics] Fix cover images

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/keyoapp/en/AgsComics.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/keyoapp/en/AgsComics.kt
@@ -1,10 +1,21 @@
 package org.koitharu.kotatsu.parsers.site.keyoapp.en
 
+import org.jsoup.nodes.Element
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.keyoapp.KeyoappParser
+import org.koitharu.kotatsu.parsers.util.styleValueOrNull
+import org.koitharu.kotatsu.parsers.util.cssUrl
 
 @MangaSourceParser("AGSCOMICS", "AgsComics", "en")
 internal class AgsComics(context: MangaLoaderContext) :
-	KeyoappParser(context, MangaParserSource.AGSCOMICS, "agrcomics.com")
+	KeyoappParser(context, MangaParserSource.AGSCOMICS, "agrcomics.com") {
+
+	override val cover: (Element) -> String? = { div ->
+		div.selectFirst("div.bg-cover, div.bg-center.bg-cover")
+			?.styleValueOrNull("background-image")
+			?.cssUrl()
+	}
+
+}
